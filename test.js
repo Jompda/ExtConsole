dcad4fb054ac4472f6edfded3f6c9d22762d85e7
Jompda <45610052+Jompda@users.noreply.github.com>
2021-07-21 20:59:54 +0300

    Test

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 const extConsole = require('./index.js')
 
 
-extConsole.then(() => {
-    extConsole.createConsole().then((c) => {
-        c.log('Number incoming', 123, 'yes.')
-        c.time('test')
-        setTimeout(() => c.timeEnd('test'), 1000)
-    })
-})
+extConsole.then(() =>
+    process.stdin.on('data', (data) =>
+        extConsole.createConsole().then((c) => {
+            console.log(`Started external console (${c.socket.remoteAddress}:${c.socket.remotePort}).`)
+            c.log(data.toString('utf8', 0, data.length - 2)) // Remove 2x newline
+        })
+    )
+)
 
